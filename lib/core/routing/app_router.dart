b15b4ec7146e3cd3461cbaec05f741b68b786bad
kartali mohamed <mohameekartali@gmail.com>
2024-10-21 16:08:36 +0100

    - added homescreen route to the approuter and routes - updated the loginscreen route in approuter to use blocprovider with logincubit from dependency injection

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_advance_project/core/di/dependency_injection.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../features/home/ui/home_screen.dart';
+import '../../features/login/logic/cubit/login_cubit.dart';
 import '../../features/login/ui/screens/login_screen.dart';
 import '../../features/onboarding/onboarding_screen.dart';
 import 'routes.dart';
@@ -13,7 +17,13 @@ class AppRouter {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => getIt<LoginCubit>(),
+                  child: const LoginScreen(),
+                ));
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
